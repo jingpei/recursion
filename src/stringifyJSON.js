441773498ea6512c9584691c7a71f39490ed77a1
jingpei <jingjpei@gmail.com>
2015-07-13 17:01:45 -0400

    Passing stringifiable objects, added underbar.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,36 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
   // your code goes here
+
+	if(typeof(obj) === 'function' || obj === undefined){
+  		return ""
+  	}
+  	else if(typeof(obj) === 'string'){
+  		return "\"" + obj + "\"" ;
+  	}
+  	else if(obj instanceof Array){ 
+		var accumString = ""
+		_.each(obj, function(arrayItem, arrayIndex){
+	  		accumString += stringifyJSON(arrayItem)
+	  		if(arrayIndex != obj.length-1){
+	  			accumString += ","
+	  		}	
+		})
+		return "[" + accumString + "]";
+  	}
+	else if(obj instanceof Object){
+		var accumString = ""
+		var lastKey = Object.keys(obj).pop()
+		_.each(obj, function(property, key){
+			accumString += stringifyJSON(key) + ":" + stringifyJSON(property)
+			if(key !== lastKey){
+				accumString += ","
+			}
+		})
+		return "{" + accumString + "}"
+	}
+  	return String(obj)
 };
